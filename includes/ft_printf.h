b54cfd857217c0b453b2cc1e8359baf87975794b
arnaudderison <arnaud.derison@hotmail.com>
2023-12-04 14:05:37 +0100

    add protorype to header

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,17 +6,20 @@
 /*   By: arnaud <arnaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 08:28:38 by arnaud            #+#    #+#             */
-/*   Updated: 2023/12/04 09:43:16 by arnaud           ###   ########.fr       */
+/*   Updated: 2023/12/04 12:35:12 by arnaud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# include <stdarg.h>
 # include <unistd.h>
 
-int	ft_putchar_fd(char c, int fd);
-int	ft_putstr_fd(char const *s, int fd);
-int	ft_pointer(size_t addr);
+int		ft_printf(const char *strs, ...);
+int		ft_putchar_fd(char c, int fd);
+int		ft_putstr_fd(char const *s, int fd);
+int		ft_pointer(size_t addr);
+size_t	ft_strlen(const char *str);
 
 #endif
