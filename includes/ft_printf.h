e41840247075c2d66a9541c979ffefb34746b130
arnaudderison <arnaud.derison@hotmail.com>
2023-12-04 09:44:00 +0100

    header

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,14 +6,17 @@
 /*   By: arnaud <arnaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 08:28:38 by arnaud            #+#    #+#             */
-/*   Updated: 2023/12/04 08:36:19 by arnaud           ###   ########.fr       */
+/*   Updated: 2023/12/04 09:43:16 by arnaud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char const *s, int fd);
+# include <unistd.h>
+
+int	ft_putchar_fd(char c, int fd);
+int	ft_putstr_fd(char const *s, int fd);
+int	ft_pointer(size_t addr);
 
 #endif
